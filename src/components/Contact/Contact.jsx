0202c3ded138a6b91e511f3e7a050fd360e8b926
NataliaShtefan <153209330+NataliaShtefan@users.noreply.github.com>
2024-07-27 19:29:26 +0200

    butoon delete

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -2,13 +2,14 @@ import s from "./Contact.module.css"
 import { FaUserAlt } from "react-icons/fa";
 import { FaPhone } from "react-icons/fa6";
 
-const Contact = ({ firstName, number}) => {
+const Contact = ({ firstName, number, id, deleteContact}) => {
   return (
     <>
     <div className={s.item}>
       <p> <FaUserAlt  className={s.icon}/> {firstName}</p>
       <p> <FaPhone className={s.icon}/> {number}</p>
       </div>
+      <button onClick={() => deleteContact(id)} className={s.btn}>Delete</button>
     </>
   )
 }
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -8,8 +8,7 @@ const ContactList = ({ contacts, deleteContact }) => {
     <ul className={s.contacts}>
       {contacts.map(contact => 
         (<li className={s.contact} key={contact.id}>
-            < Contact firstName={contact.name} number={contact.number} deleteContact={deleteContact}/>
-            <button onClick={() => deleteContact(contact.id)} className={s.btn}>Delete</button>
+            < Contact firstName={contact.name} number={contact.number} id={contact.id} deleteContact={deleteContact}/>
         </li>)
       )}
     </ul>
